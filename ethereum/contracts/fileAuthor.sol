5219b83ab55ffa4c2faaf06071dad4e044c222a3
sreeteja06 <sreeteja.muthyala@gmail.com>
2019-01-13 15:31:53 +0530

    moved mapping to sruct in the contract

--- ethereum/contracts/fileAuthor.sol
+++ ethereum/contracts/fileAuthor.sol
@@ -1,29 +1,32 @@
 pragma solidity ^0.5.0;
 
 contract fileAuthor{
-    mapping (string=>uint) fileTimeStamp;
-    mapping (string=>address) fileOwner;
-    mapping (address=>uint) number_of_files_owner_owns;
-    function checkForFile(string memory fileHash) public view returns (bool){
-        if(fileOwner[fileHash]==address(0x0)){
-            return false;
-        }
-        else{
-            return true;
-        }
+    struct FileProperties{
+        uint timeStamp;
+        address owner;
+        bool flag;
     }
+    mapping (string=>FileProperties) savedFilesProperties;
+    mapping (address=>uint) number_of_files_owner_owns;
+    
+    function checkForFile(string memory fileHash) public view returns (bool){
+        return savedFilesProperties[fileHash].flag;
+    }
+    
     function addFile(string memory fileHash) public returns (bool){
-        require((address(0x0)==fileOwner[fileHash]),"checks if the file already exists");
-        fileOwner[fileHash] = msg.sender;
-        fileTimeStamp[fileHash] = now;
+        require(!savedFilesProperties[fileHash].flag,"checks if the file already exists");
+        savedFilesProperties[fileHash].flag = true;
+        savedFilesProperties[fileHash].owner = msg.sender;
+        savedFilesProperties[fileHash].timeStamp = now;
+        number_of_files_owner_owns[msg.sender]++;
         return true;
     }
     function getOwner(string memory fileHash) public view returns (address){
-        require((address(0x0)!=fileOwner[fileHash]),"checks if the file exists");
-        return fileOwner[fileHash];
+        require(savedFilesProperties[fileHash].flag,"checks if the file exists");
+        return savedFilesProperties[fileHash].owner;
     }
     function getFileTimeStamp(string memory fileHash) public view returns (uint){
-        require((address(0x0)!=fileOwner[fileHash]),"checks if the file exists");
-        return fileTimeStamp[fileHash];
+        require(savedFilesProperties[fileHash].flag,"checks if the file exists");
+        return savedFilesProperties[fileHash].timeStamp;
     }
 }
