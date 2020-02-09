4ab5426874729078397d144834f998f1838b5749
Sree Teja <smutyala@nvestbank.com>
2020-02-09 12:25:37 +0530

    bug fix

--- src/components/DigNotary/DigNotary.js
+++ src/components/DigNotary/DigNotary.js
@@ -8,194 +8,198 @@
  * Author: SreeTeja06 (sreeteja.muthyala@gmail.com)
 
  */
-import React, { Component } from "react";
-import fileAuthorContract from "../../contractHelper/fileAuthor";
-import web3 from "../../contractHelper/web3";
-import FileUpload from "../FileUpload/FileUpload";
-import UserFilesList from "../UserFilesList/UserFileList";
-import { Loader, Message, Card } from "semantic-ui-react";
+import React, { Component } from 'react';
+import fileAuthorContract from '../../contractHelper/fileAuthor';
+import web3 from '../../contractHelper/web3';
+import FileUpload from '../FileUpload/FileUpload';
+import UserFilesList from '../UserFilesList/UserFileList';
+import { Loader, Message, Card } from 'semantic-ui-react';
 import NavBar from '../navbar/Navbar';
-import { protocols } from "multiaddr";
-let ipfsClient = require( 'ipfs-http-client' );
+import { protocols } from 'multiaddr';
+let ipfsClient = require('ipfs-http-client');
 
 class DigNotary extends Component {
-    hash = "";
-    accounts = [];
-    state = {
-        errMessage: null,
-        owner: "",
-        timeStamp: "",
-        loading: null,
-        showFileDetails: false
-    };
+  hash = '';
+  accounts = [];
+  state = {
+    errMessage: null,
+    owner: '',
+    timeStamp: '',
+    loading: null,
+    showFileDetails: false
+  };
 
-    saveFile = file => {
-        this.file = file
-    }
-    //ipfs.infura.io protocol https
-    generateFileHash = async () => {
-        let host = "localhost";
-        // host = "ipfs.infura.io"
-        // let ipfs = ipfsClient({ host, port:'5001', protocol: 'https' })
-        let ipfs = ipfsClient( { host, port: '5001' } )
-        this.fetchAccounts();
-        const files = [
-            {
-                path: "/tmp/" + this.file.name,
-                content: this.file
-            }
-        ]
-        this.setState( { loading: "uploading file to ipfs" } );
-        let result = await ipfs.add( files )
-        console.log( "result of ipfs file upload" + JSON.stringify( result ) );
-        this.setState( { loading: null } )
-        this.hash = result[1].hash;
-    };
-    submit = async () => {
-        this.setState( {
-            errMessage: null,
-            owner: "",
-            timeStamp: "",
-            loading: null,
-            showFileDetails: false
-        } );
-        await this.generateFileHash();
+  saveFile = file => {
+    this.file = file;
+  };
+  //ipfs.infura.io protocol https
+  generateFileHash = async () => {
+    let host = 'localhost';
+    // host = "ipfs.infura.io"
+    // let ipfs = ipfsClient({ host, port:'5001', protocol: 'https' })
+    let ipfs = ipfsClient({ host, port: '5001' });
+    this.fetchAccounts();
+    const files = [
+      {
+        path: '/tmp/' + this.file.name,
+        content: this.file
+      }
+    ];
+    this.setState({ loading: 'uploading file to ipfs' });
+    let result = await ipfs.add(files);
+    console.log('result of ipfs file upload' + JSON.stringify(result));
+    this.setState({ loading: null });
+    this.hash = result[1].hash;
+  };
+  submit = async () => {
+    this.setState({
+      errMessage: null,
+      owner: '',
+      timeStamp: '',
+      loading: null,
+      showFileDetails: false
+    });
+    await this.generateFileHash();
 
-        const flag = await this.checkForFile();
-        console.log( "Check for life returned:" + flag );
-        if ( !flag ) {
-            await this.addFileToContract();
-        } else {
-            console.log( `the file is already in the contract
+    const flag = await this.checkForFile();
+    console.log('Check for life returned:' + flag);
+    if (!flag) {
+      await this.addFileToContract();
+    } else {
+      console.log(`the file is already in the contract
       `);
-            this.setState( {
-                errMessage: (
-                    <Message warning>
-                        <Message.Header>the file is already in the contract at </Message.Header>
-                    </Message>
-                )
-            } );
-            await this.fileDetails();
-        }
-    };
-
-    checkForFile = async () => {
-        console.log( "check for file called" );
-        const flag = await fileAuthorContract.methods
-            .checkForFile( this.hash )
-            .call();
-        console.log( flag );
-        return flag;
-    };
-
-    fetchAccounts = async () => {
-        try {
-            this.accounts = await web3.eth.getAccounts();
-        } catch ( e ) {
-            console.log( "cannot get accounts" );
-        }
-    };
-
-    addFileToContract = async () => {
-        console.log( "AddFileToContractCalled" );
-        this.setState( { loading: "waiting for transaction to be completed" } );
-        try {
-            const flag = await fileAuthorContract.methods.addFile( this.hash ).call( {
-                from: this.accounts[0]
-            } );
-            console.log( "call executed" + flag );
-            const reciept = await fileAuthorContract.methods.addFile( this.hash ).send( {
-                from: this.accounts[0]
-            } );
-            console.log( "send the transaction: " + reciept );
-            if ( flag ) {
-                console.log( "the file is saved into the contract" );
-                this.setState( { message1: "the file is saved into the contract" } );
-            }
-        } catch ( e ) {
-            for ( let key in e.results ) {
-                //checks for revert condition
-                if ( e.results[key]["reason"] !== undefined ) {
-                    if (
-                        e.results[key]["reason"] === "checks if the file already exists"
-                    ) {
-                        console.log( "the file already exists" );
-                    }
-                }
-            }
-        }
-        console.log( "Done Adding file to the contract" );
-        this.setState( { loading: null } );
-    };
-
-    fileDetails = async () => {
-        const FileDetails = await fileAuthorContract.methods
-            .getFileDetails( this.hash )
-            .call();
-        console.log( "Owner:" + FileDetails.owner );
-        console.log( "timeStamp:" + FileDetails.timeStamp );
-        const date = new Date( FileDetails.timeStamp * 1000 );
-        const formatedDate = this.GetFormattedDate( date );
-        this.setState( {
-            showFileDetails: true,
-            address: `https://gateway.ipfs.io/ipfs/${ this.hash }`,
-            owner: "Owner: " + FileDetails.owner,
-            timeStamp: "timeStamp:" + formatedDate
-        } );
-    };
-
-    GetFormattedDate = date => {
-        const monthNames = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-        ];
-        var month = date.getMonth();
-        var day = date.getDate();
-        var year = date.getFullYear();
-        return day + "/" + monthNames[month] + "/" + year;
-    };
-
-    render() {
-        return (
-            <div>
-                <NavBar navItem="notary"></NavBar>
-                <h2>Digital Notary</h2>
-                <br />
-                <div style={{ "marginTop": "100px" }}>
-                    <FileUpload
-                        change={e => this.saveFile( e.target.files[0] )}
-                        click={this.submit}
-                    />
-                </div>
-                <div>
-                    {this.state.errMessage}
-                    <Loader active={this.state.loading} inline="centered">
-                        {this.state.loading}
-                    </Loader>
-                    {this.state.showFileDetails ?
-                        <Card fluid>
-                            <Card.Content header="About File" />
-                            <Card.Content description={this.state.owner} />
-                            <Card.Content extra>{this.state.timeStamp}</Card.Content>
-                            <a href={this.state.address}>check the file</a>
-                        </Card> : null}
-                </div>
-                <div style={{ "marginTop": "150px", "marginBottom": "130px" }}>
-                    <UserFilesList dateFormatter={this.GetFormattedDate}></UserFilesList>
-                </div>
-            </div>
-        );
+      this.setState({
+        errMessage: (
+          <Message warning>
+            <Message.Header>
+              the file is already in the contract at{' '}
+            </Message.Header>
+          </Message>
+        )
+      });
+      await this.fileDetails();
     }
+  };
+
+  checkForFile = async () => {
+    console.log('check for file called');
+    const flag = await fileAuthorContract.methods
+      .checkForFile(this.hash)
+      .call();
+    console.log(flag);
+    return flag;
+  };
+
+  fetchAccounts = async () => {
+    try {
+      this.accounts = await web3.eth.getAccounts();
+      await window['ethereum'].enable();
+    } catch (e) {
+      console.log('cannot get accounts');
+    }
+  };
+
+  addFileToContract = async () => {
+    console.log('AddFileToContractCalled');
+    this.setState({ loading: 'waiting for transaction to be completed' });
+    try {
+      const flag = await fileAuthorContract.methods.addFile(this.hash).call({
+        from: this.accounts[0]
+      });
+      console.log('call executed' + flag);
+      const reciept = await fileAuthorContract.methods.addFile(this.hash).send({
+        from: this.accounts[0]
+      });
+      console.log('send the transaction: ' + reciept);
+      if (flag) {
+        console.log('the file is saved into the contract');
+        this.setState({ message1: 'the file is saved into the contract' });
+      }
+    } catch (e) {
+      for (let key in e.results) {
+        //checks for revert condition
+        if (e.results[key]['reason'] !== undefined) {
+          if (
+            e.results[key]['reason'] === 'checks if the file already exists'
+          ) {
+            console.log('the file already exists');
+          }
+        }
+      }
+    }
+    console.log('Done Adding file to the contract');
+    this.setState({ loading: null });
+  };
+
+  fileDetails = async () => {
+    const FileDetails = await fileAuthorContract.methods
+      .getFileDetails(this.hash)
+      .call();
+    console.log('Owner:' + FileDetails.owner);
+    console.log('timeStamp:' + FileDetails.timeStamp);
+    const date = new Date(FileDetails.timeStamp * 1000);
+    const formatedDate = this.GetFormattedDate(date);
+    this.setState({
+      showFileDetails: true,
+      address: `https://gateway.ipfs.io/ipfs/${this.hash}`,
+      owner: 'Owner: ' + FileDetails.owner,
+      timeStamp: 'timeStamp:' + formatedDate
+    });
+  };
+
+  GetFormattedDate = date => {
+    const monthNames = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    var month = date.getMonth();
+    var day = date.getDate();
+    var year = date.getFullYear();
+    return day + '/' + monthNames[month] + '/' + year;
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar navItem='notary'></NavBar>
+        <h2>Digital Notary</h2>
+        <br />
+        <div style={{ marginTop: '100px' }}>
+          <FileUpload
+            change={e => this.saveFile(e.target.files[0])}
+            click={this.submit}
+          />
+        </div>
+        <div>
+          {this.state.errMessage}
+          <Loader active={this.state.loading} inline='centered'>
+            {this.state.loading}
+          </Loader>
+          {this.state.showFileDetails ? (
+            <Card fluid>
+              <Card.Content header='About File' />
+              <Card.Content description={this.state.owner} />
+              <Card.Content extra>{this.state.timeStamp}</Card.Content>
+              <a href={this.state.address}>check the file</a>
+            </Card>
+          ) : null}
+        </div>
+        <div style={{ marginTop: '150px', marginBottom: '130px' }}>
+          <UserFilesList dateFormatter={this.GetFormattedDate}></UserFilesList>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DigNotary;
--- src/components/UserFilesList/UserFileList.js
+++ src/components/UserFilesList/UserFileList.js
@@ -8,107 +8,114 @@
  * Author: SreeTeja06 (sreeteja.muthyala@gmail.com)
 
  */
-import { Button, Loader, Icon, Table } from "semantic-ui-react";
-import fileAuthorContract from "../../contractHelper/fileAuthor";
-import web3 from "../../contractHelper/web3";
+import { Button, Loader, Icon, Table } from 'semantic-ui-react';
+import fileAuthorContract from '../../contractHelper/fileAuthor';
+import web3 from '../../contractHelper/web3';
 
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
-class UserFileList extends Component{
-    accounts = [];
-    table = [];
-    files = [];
-    state = {
-        showFiles: false,
-        loading: null
+class UserFileList extends Component {
+  accounts = [];
+  table = [];
+  files = [];
+  state = {
+    showFiles: false,
+    loading: null
+  };
+  fetchAccounts = async () => {
+    try {
+      this.accounts = await web3.eth.getAccounts();
+      await window['ethereum'].enable()[0];
+    } catch (e) {
+      console.log('cannot get accounts');
     }
-    fetchAccounts = async () => {
-        try {
-            this.accounts = await web3.eth.getAccounts();
-        } catch ( e ) {
-            console.log( "cannot get accounts" );
-        }
+  };
+
+  getFiles = async () => {
+    this.setState({
+      showFiles: false,
+      loading: 'Hold your horses, Fetching User Files.'
+    });
+    await this.fetchAccounts();
+    let nFiles = await this.getNumberOfUserFiles();
+    for (let i = 0; i < nFiles; i++) {
+      this.files.push(await this.getUserFile(i));
+    }
+    this.table = [];
+    for (let j = 0; j < this.files.length; j++) {
+      let hrefValue = `https://gateway.ipfs.io/ipfs/${this.files[j].hash}`;
+      let signHref = `/digdocsign?fileHash=${this.files[j].hash}`;
+      let date = this.props.dateFormatter(
+        new Date(this.files[j].timeStamp * 1000)
+      );
+      //http://localhost:3000/digdocsign?fileHash=QmPmtUizVmtYXauQEPZATAjo76tvPeAnaAxYFejcE63374
+      this.table.push(
+        <Table.Row>
+          <Table.Cell collapsing>
+            <Icon name='file' />
+          </Table.Cell>
+          <Table.Cell>
+            <a href={hrefValue}>{this.files[j].hash}</a> click
+          </Table.Cell>
+          <Table.Cell>{date}</Table.Cell>
+          <Table.Cell>
+            <NavLink to={signHref}>
+              <Button color='blue'>Sign Link</Button>
+            </NavLink>
+          </Table.Cell>
+        </Table.Row>
+      );
+    }
+    this.setState({
+      showFiles: true,
+      loading: null
+    });
+  };
+
+  getNumberOfUserFiles = async () => {
+    const flag = await fileAuthorContract.methods.getNumberOfUserFiles().call({
+      from: this.accounts[0]
+    });
+    console.log('number of user files: ' + flag);
+    return flag;
+  };
+
+  getUserFile = async index => {
+    const file = await fileAuthorContract.methods.getUserFile(index).call({
+      from: this.accounts[0]
+    });
+    return {
+      hash: file.fileHash,
+      timeStamp: file.timeStamp
     };
+  };
 
-    getFiles = async () => {
-        this.setState( {
-            showFiles: false,
-            loading: "Hold your horses, Fetching User Files."
-        })
-        await this.fetchAccounts();
-        let nFiles = await this.getNumberOfUserFiles();
-        for( let i = 0; i < nFiles ; i++){
-            this.files.push(await this.getUserFile(i));
-        }
-        this.table = [];
-        for(let j = 0; j < this.files.length ; j++){
-            let hrefValue = `https://gateway.ipfs.io/ipfs/${ this.files[j].hash}`
-            let signHref = `/digdocsign?fileHash=${this.files[j].hash}`
-            let date = this.props.dateFormatter(new Date(this.files[j].timeStamp * 1000))
-            //http://localhost:3000/digdocsign?fileHash=QmPmtUizVmtYXauQEPZATAjo76tvPeAnaAxYFejcE63374
-            this.table.push(
-                <Table.Row>
-                    <Table.Cell collapsing>
-                        <Icon name='file' /> 
-                    </Table.Cell>
-                    <Table.Cell><a href = {hrefValue} >{this.files[j].hash}</a>    click</Table.Cell>
-                    <Table.Cell>{date}</Table.Cell>
-                    <Table.Cell><NavLink to={signHref}><Button color="blue">Sign Link</Button></NavLink></Table.Cell>
-                </Table.Row>
-            )
-        }
-        this.setState({
-            showFiles: true,
-            loading: null
-        })
-    }
-
-    getNumberOfUserFiles = async () => {
-        const flag = await fileAuthorContract.methods
-            .getNumberOfUserFiles( )
-            .call( {
-                from: this.accounts[0]
-            });
-        console.log( "number of user files: "+flag );
-        return flag;
-    };
-
-    getUserFile = async (index) => {
-        const file = await fileAuthorContract.methods
-            .getUserFile(index)
-            .call({
-                from: this.accounts[0]
-            });
-        return {
-            hash: file.fileHash,
-            timeStamp: file.timeStamp
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <Button color='teal' onClick={this.getFiles}>Show User Files List</Button>
-                <Loader active={this.state.loading} inline="centered">
-                    {this.state.loading}
-                </Loader>
-                {this.state.showFiles?
-                    <Table celled striped>
-                        <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell colSpan='3'><div style={{ "color": "#EE694C" }}>{this.accounts[0]}</div> ACCOUNT HOLDER Files</Table.HeaderCell>
-                            </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                        {this.table}
-                        </Table.Body>
-                    </Table>
-                    :null
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Button color='teal' onClick={this.getFiles}>
+          Show User Files List
+        </Button>
+        <Loader active={this.state.loading} inline='centered'>
+          {this.state.loading}
+        </Loader>
+        {this.state.showFiles ? (
+          <Table celled striped>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell colSpan='3'>
+                  <div style={{ color: '#EE694C' }}>{this.accounts[0]}</div>{' '}
+                  ACCOUNT HOLDER Files
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>{this.table}</Table.Body>
+          </Table>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default UserFileList;
